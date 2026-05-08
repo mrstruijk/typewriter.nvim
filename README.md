27d5b6e1b54027dd5a4ea611e663694832342743
mrstruijk <mrstruijk@gmail.com>
2026-05-08 10:06:42 +0200

    First public version

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # typewriter.nvim
+
+> Center the cursor vertically like a typewriter
+
+The `position` is a number between `0` and `1`:
+
+- `0.05` near top
+- `0.25` high up
+- `0.50` centered
+- `0.75` very low
+
+If `immediate` is false, it waits until the first keystroke to recenter cursor position.
+
+```lua
+return {
+  url="mrstruijk/typewriter.nvim",
+
+  cmd = { "TypeWriter" },
+
+  opts = {
+    position = 0.06,
+    immediate = true,
+  },
+
+  keys = {
+    {
+      "<leader>zz",
+      "<cmd>TypeWriter<CR>",
+      desc = "Toggle Typewriter",
+    },
+  },
+}
+```
---
+++ lua/typewriter/init.lua
@@ -0,0 +1,75 @@
+-- lua/typewriter/init.lua
+-- Keeps the cursor at set vertical location in buffer.
+-- Toggle via `TypeWriter` command
+
+local M = {}
+
+M.enabled = false
+M.augroup = vim.api.nvim_create_augroup("TypewriterScroll", { clear = true })
+
+M.opts = {
+	position = 0.05,
+	immediate = true, -- if false then it will wait until the first keystroke
+}
+
+-- scroll to a particular position on screen
+local function scroll(position)
+	local win_height = vim.fn.winheight(0)
+	local target = math.floor(win_height * position)
+
+	local line = vim.fn.line(".")
+	local top = math.max(line - target, 1)
+
+	vim.fn.winrestview({
+		topline = top,
+	})
+end
+
+function M.enable()
+	if M.enabled then
+		return
+	end
+	vim.api.nvim_create_autocmd("CursorMoved", {
+		group = M.augroup,
+		callback = function()
+			scroll(M.opts.position)
+		end,
+	})
+
+	if M.opts.immediate then
+		scroll(M.opts.position)
+	end
+
+	M.enabled = true
+	vim.notify("Typewriter mode ON")
+end
+
+function M.disable()
+	if M.disabled then
+		return
+	end
+	vim.api.nvim_clear_autocmds({
+		group = M.augroup,
+	})
+
+	M.enabled = false
+	vim.notify("Typewriter mode OFF")
+end
+
+function M.toggle()
+	if M.enabled then
+		M.disable()
+	else
+		M.enable()
+	end
+end
+
+function M.setup(opts)
+	M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
+
+	vim.api.nvim_create_user_command("TypeWriter", function()
+		M.toggle()
+	end, {})
+end
+
+return M
